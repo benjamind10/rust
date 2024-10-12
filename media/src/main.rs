b80eaf60395d909e880c5e16292208f7c7cddcb6
benjamind10 <benjamind10@pm.me>
2024-10-12 12:24:37 -0400

    added media project: enums

--- media/src/main.rs
+++ media/src/main.rs
@@ -1,14 +1,24 @@
 #[derive(Debug)]
 enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
+    Book {
+        title: String,
+        author: String,
+    },
+    Movie {
+        title: String,
+        director: String,
+    },
+    Audiobook {
+        title: String,
+    },
+    Podcast(u32),
+    Placeholder,
 }
 
 impl Media {
     fn description(&self) -> String {
         //if let Media::Book { title, author } = self {
-        //format!("Book: {} by {}", title, author)
+        //format!("Book: {} by {}", titwle, author)
         //} else if let Media::Movie { title, director } = self {
         //format!("Movie: {} directed by {}", title, director)
         //} else if let Media::Audiobook { title } = self {
@@ -17,22 +27,48 @@ impl Media {
         //String::from("Unknown media type")
         //}
         match self {
-            Media::Book { title, author } => {
-                format!("Book: {} by {}", title, author)
-            }
+            Media::Book { title, author } => { format!("Book: {} by {}", title, author) }
             Media::Movie { title, director } => {
                 format!("Movie: {} directed by {}", title, director)
             }
-            Media::Audiobook { title } => {
-                format!("Audiobook: {}", title)
-            }
+            Media::Audiobook { title } => { format!("Audiobook: {}", title) }
+            Media::Podcast(id) => { format!("Podcast: {}", id) }
+            Media::Placeholder => { format!("Placeholder") }
         }
     }
 }
 
-fn print_media(media: Media) {
-    println!("{}", media.description());
+#[derive(Debug)]
+struct Catalog {
+    items: Vec<Media>,
 }
+
+impl Catalog {
+    fn new() -> Self {
+        Catalog { items: vec![] }
+    }
+
+    fn add(&mut self, media: Media) {
+        self.items.push(media);
+    }
+    fn get_by_index(&self, index: usize) -> Option<&Media> {
+        if self.items.len() > index {
+            Some(&self.items[index])
+        } else {
+            // We don't have anything to return
+            None
+        }
+    }
+}
+
+// enum MightHaveAValue<'a> {
+//     ThereIsAValue(&'a Media),
+//     NoValueAvailable,
+// }
+
+// fn print_media(media: Media) {
+//     println!("{}", media.description());
+// }
 
 fn main() {
     let audiobook = Media::Audiobook {
@@ -48,8 +84,20 @@ fn main() {
         title: String::from("Inception"),
         director: String::from("Christopher Nolan"),
     };
+    let podcast = Media::Podcast(10);
+    let placeholder = Media::Placeholder;
 
-    print_media(audiobook);
-    print_media(book);
-    print_media(movie);
+    let mut catalog = Catalog::new();
+    catalog.add(audiobook);
+    catalog.add(book);
+    catalog.add(movie);
+    catalog.add(podcast);
+    catalog.add(placeholder);
+
+    let item = catalog.get_by_index(0);
+    let placeholder = Media::Placeholder;
+
+    // println!("{:#?}", item.unwrap())
+    // println!("{:#?}", item.expect("expected there to be an item here!"))
+    println!("{:#?}", item.unwrap_or(&placeholder))
 }
