c72d66985e37cc3ae56627387ef6f8d719db0a66
benjamind10 <benjamind10@pm.me>
2024-10-15 16:40:46 -0400

    generics project

---
+++ generics/src/main.rs
@@ -0,0 +1,15 @@
+use num_traits::{ Float, ToPrimitive };
+
+fn solve<T: Float>(a: T, b: T) -> f64 {
+    let a_f64 = a.to_f64().unwrap();
+    let b_f64 = b.to_f64().unwrap();
+
+    (a_f64.powi(2) + b_f64.powi(2)).sqrt()
+}
+
+fn main() {
+    let a: f32 = 3.0;
+    let b: f32 = 4.0;
+
+    println!("{}", solve::<f32>(a, b));
+}
